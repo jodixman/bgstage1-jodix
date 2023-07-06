216187c07f29c977e7fbef42b24372f4d78328b3
Setiawan Jodi <setiawan.jodi38@gmail.com>
2023-07-07 00:02:17 +0700

    commit 3

---
+++ js/darkmode.js
@@ -0,0 +1,23 @@
+const navbarLogo = document.querySelector('.navbar-logo');
+navbarLogo.addEventListener('click', toggleDarkMode);
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+  navbarLogo.querySelector('i').classList.toggle('fa-moon');
+  navbarLogo.querySelector('i').classList.toggle('fa-sun');
+
+  // Menyimpan status dark mode saat toggle di klik
+  const isDarkMode = document.body.classList.contains('dark-mode');
+  saveDarkModePreference(isDarkMode);
+}
+
+// Memeriksa status dark mode saat halaman dimuat
+document.addEventListener('DOMContentLoaded', () => {
+  const isDarkMode = localStorage.getItem('darkMode') === 'true';
+  toggleDarkMode(isDarkMode);
+});
+
+// Menyimpan status dark mode saat toggle di klik
+function saveDarkModePreference(isDarkMode) {
+  localStorage.setItem('darkMode', isDarkMode);
+}
